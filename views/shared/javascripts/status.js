2cbf93390905a3d6feee9bcd27e1555eb4aeae6b
Dickson Law <dickson.law@utoronto.ca>
2018-02-13 17:28:54 -0500

    Refresh the daemon status automatically

--- views/shared/javascripts/status.js
+++ views/shared/javascripts/status.js
@@ -1,4 +1,10 @@
 jQuery(function() {
+    var refreshDaemonStatus = function() {
+        jQuery.get(jQuery('#refresh_daemon_status').data('url')).done(function(data) {
+            jQuery('#daemon_status').html(data['daemon_status']);
+        });
+        return false;
+    };
     
     // Handle "Refresh" button under authentication status
     jQuery('#refresh_auth_status').on('click', function() {
@@ -9,11 +15,8 @@ jQuery(function() {
     });
     
     // Handle "Refresh" button under authentication status
-    jQuery('#refresh_daemon_status').on('click', function() {
-        jQuery.get(jQuery(this).data('url')).done(function(data) {
-            jQuery('#daemon_status').html(data['daemon_status']);
-        });
-        return false;
-    });
+    jQuery('#refresh_daemon_status').on('click', refreshDaemonStatus);
     
+    // Refresh daemon status every 5 seconds
+    setInterval(refreshDaemonStatus, 5000);
 });
